21455d23858020e103e847fd91b9b454d7827906
Lavishmehta <Lavish@Compro>
2020-08-11 19:07:52 +0530

    chrome version

--- tests/healthCheck.js
+++ tests/healthCheck.js
@@ -34,21 +34,22 @@ describe('IELTS - Health Check Test', function() {
       .waitForElementVisible(progressPage.elements.readingTest, config[testEnv].timeout, "Checking Reading Tile is expanded")
       .click(progressPage.elements.readingTest)
       .waitForElementVisible(productLaunchPage.elements.testContainer, config[testEnv].timeout, 'Step 4: Launch IETLS Test Page (Checking Container is painted)')
-      // .frame(0)
-      // .waitForElementVisible(productLaunchPage.elements.answerOption2, config[testEnv].timeout, "Step 5: Validating Metrica Frame & Test Content")
-      // .waitForElementVisible(productLaunchPage.elements.currentQuestionCount, config[testEnv].timeout, "Step 6: Metrica Score Progress Layer")
-      // .assert.containsText(productLaunchPage.elements.currentQuestionCount, config[testEnv].student.currentQuestionCount, "Checking Current Score is matching")
-      // .assert.containsText(productLaunchPage.elements.totalQuestionCount, config[testEnv].student.totalQuestionCount, "Checking Total Score is matching")
-      // .frameParent()
-      // .click(dashboardPage.elements.userProfileName)
-      // .click(dashboardPage.elements.logoutButton)
-      // .waitForElementVisible(homePage.elements.heading, config[testEnv].timeout, "Logged Out successfully")
+      .frame(0)
+      .waitForElementVisible(productLaunchPage.elements.answerOption2, config[testEnv].timeout, "Step 5: Validating Metrica Frame & Test Content")
+      .waitForElementVisible(productLaunchPage.elements.currentQuestionCount, config[testEnv].timeout, "Step 6: Metrica Score Progress Layer")
+      .assert.containsText(productLaunchPage.elements.currentQuestionCount, config[testEnv].student.currentQuestionCount, "Checking Current Score is matching")
+      .assert.containsText(productLaunchPage.elements.totalQuestionCount, config[testEnv].student.totalQuestionCount, "Checking Total Score is matching")
+      .frameParent()
+      .click(dashboardPage.elements.userProfileName)
+      .click(dashboardPage.elements.logoutButton)
+      .waitForElementVisible(homePage.elements.heading, config[testEnv].timeout, "Logged Out successfully")
+      .end()
   });
 
-  after(function(browser, done) {
-    browser.end(function() {
-      done();
-    });
-  });
+  // after(function(browser, done) {
+  //   browser.end(function() {
+  //     done();
+  //   });
+  // });
 
 });
