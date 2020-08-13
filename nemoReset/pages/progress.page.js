69c2c8434f7a28fb8874d74d5bd91377c8eae68c
Lavishmehta <Lavish@Compro>
2020-08-13 10:07:14 +0000

    Added testcase

--- nemoReset/pages/progress.page.js
+++ nemoReset/pages/progress.page.js
@@ -17,14 +17,14 @@ module.exports = {
         {
             clickReadingTile: function(){
                 this.api.useXpath();
-                actions.waitForElementVisible(this,this.elements.username.readingTile,30000);
+                actions.waitForElementVisible(this,this.elements.readingTile.selector,30000);
                 actions.click(this,this.elements.readingTile.selector);
                 this.api.useCss();
             },
 
 			clickExpandedReadingTile: function(){
                 this.api.useXpath();
-                actions.waitForElementVisible(this,this.elements.username.readingTest,30000);
+                actions.waitForElementVisible(this,this.elements.readingTest.selector,30000);
                 actions.click(this,this.elements.readingTest.selector);
                 this.api.useCss();
             }            
